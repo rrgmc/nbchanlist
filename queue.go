b7ed91bb58c9612576456ea80994650d67efb31c
Rangel Reale <rangelspam@gmail.com>
2025-07-21 12:14:47 -0300

    rename to List

--- queue.go
+++ queue.go
@@ -1,87 +1,39 @@
 package nbchanqueue
 
-import (
-	"context"
-	"errors"
-	"sync/atomic"
-)
-
-var (
-	ErrClosed = errors.New("queue is closed")
-)
-
-// Queue is a non-blocking unbounded lock-free channel-based queue for Golang.
-type Queue[E any, Q ListType[E]] struct {
-	out  <-chan E
-	in   atomic.Pointer[chan<- E]
-	info <-chan queueChanInfo
+// NewQueue returns a List using a queue implementation.
+func NewQueue[E any]() *List[E, *ListQueue[E]] {
+	return New(NewListQueue[E]())
 }
 
-func New[E any]() *Queue[E, *ListQueue[E]] {
-	return New2(&ListQueue[E]{})
+// ListQueue is a queue implementation for ListType.
+type ListQueue[T any] struct {
+	q []T
 }
 
-func New2[E any, Q ListType[E]](list Q) *Queue[E, Q] {
-	out, in, info := newQueueChan[E](list)
-	ret := &Queue[E, Q]{
-		out:  out,
-		info: info,
+func NewListQueue[T any]() *ListQueue[T] {
+	return &ListQueue[T]{}
+}
+
+var _ ListType[int] = (*ListQueue[int])(nil)
+
+func (l *ListQueue[T]) Empty() bool {
+	return len(l.q) == 0
+}
+
+func (l *ListQueue[T]) Put(t T) {
+	l.q = append(l.q, t)
+}
+
+func (l *ListQueue[T]) Peek() (T, bool) {
+	if len(l.q) > 0 {
+		return l.q[0], true
 	}
-	ret.in.Store(&in)
-	return ret
+	var t T
+	return t, false
 }
 
-// Get returns a channel to get items. The caller must check for it to be closed.
-func (q *Queue[E, Q]) Get() <-chan E {
-	return q.out
-}
-
-// GetCtx returns one item, or an error if the context is done or the queue is closed.
-func (q *Queue[E, Q]) GetCtx(ctx context.Context) (E, error) {
-	select {
-	case <-ctx.Done():
-		var ret E
-		return ret, context.Cause(ctx)
-	case v, ok := <-q.Get():
-		if !ok {
-			var ret E
-			return ret, ErrClosed
-		}
-		return v, nil
-	}
-}
-
-// Put puts an element in the queue. It never fails or blocks.
-func (q *Queue[E, Q]) Put(e E) {
-	_ = q.PutCheck(e)
-}
-
-// PutCheck puts an element in the queue. It never fails or blocks.
-// Returns ErrClosed if the queue is closed.
-func (q *Queue[E, Q]) PutCheck(e E) error {
-	if in := q.in.Load(); in != nil {
-		*in <- e
-		return nil
-	}
-	return ErrClosed
-}
-
-func (q *Queue[E, Q]) Size() int {
-	select {
-	case info, ok := <-q.info:
-		if !ok {
-			return -1
-		}
-		return info.itemAmount
-	}
-}
-
-func (q *Queue[E, Q]) Closed() bool {
-	return q.in.Load() == nil
-}
-
-func (q *Queue[E, Q]) Close() {
-	if old := q.in.Swap(nil); old != nil {
-		close(*old)
+func (l *ListQueue[T]) Pop() {
+	if len(l.q) > 0 {
+		l.q = l.q[1:]
 	}
 }
