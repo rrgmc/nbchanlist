6ab8b56e7c8e11baec7a09f3ac65fdfff9a56690
Rangel Reale <rangelspam@gmail.com>
2025-07-21 10:11:53 -0300

    initial import

--- queue_test.go
+++ queue_test.go
@@ -13,9 +13,35 @@ func TestQueue(t *testing.T) {
 	q.Put(12)
 	q.Put(13)
 	assert.Equal(t, q.Size(), 2)
-	items, err := readNWithTimeout(q, 2)
+	assertItems(t, q, []int{12, 13})
+}
+
+func TestQueueClose(t *testing.T) {
+	q := New[int]()
+	q.Put(12)
+	q.Put(13)
+	q.Close()
+	q.Put(16)
+	expected := []int{12, 13}
+	items, err := readNWithTimeout(q, 3)
+	assert.ErrorIs(t, err, errTestClosed)
+	assert.DeepEqual(t, expected, items)
+}
+
+func TestQueueTimeout(t *testing.T) {
+	q := New[int]()
+	q.Put(12)
+	q.Put(13)
+	expected := []int{12, 13}
+	items, err := readNWithTimeout(q, 3)
+	assert.ErrorIs(t, err, errTestTimeout)
+	assert.DeepEqual(t, expected, items)
+}
+
+func assertItems[E any](t *testing.T, q *Queue[E], expected []E) {
+	items, err := readNWithTimeout(q, len(expected))
 	assert.NilError(t, err)
-	assert.DeepEqual(t, []int{12, 13}, items)
+	assert.DeepEqual(t, expected, items)
 }
 
 func readNWithTimeout[E any](q *Queue[E], n int) ([]E, error) {
@@ -24,12 +50,17 @@ func readNWithTimeout[E any](q *Queue[E], n int) ([]E, error) {
 		select {
 		case item, ok := <-q.Get():
 			if !ok {
-				return nil, errors.New("channel closed")
+				return ret, errTestClosed
 			}
 			ret = append(ret, item)
 		case <-time.After(1 * time.Second):
-			return nil, errors.New("timeout")
+			return ret, errTestTimeout
 		}
 	}
 	return ret, nil
 }
+
+var (
+	errTestClosed  = errors.New("channel closed")
+	errTestTimeout = errors.New("timeout")
+)
